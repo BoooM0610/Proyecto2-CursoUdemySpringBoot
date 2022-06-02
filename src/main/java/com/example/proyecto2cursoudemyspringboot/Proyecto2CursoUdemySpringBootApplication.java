9ec5f876808cec95f776f49d47470eaa8f23249a
BoooM0610 <eduardomartinezoc@gmail.com>
2022-06-02 08:48:05 +0200

    Initial commit

---
+++ src/main/java/com/example/proyecto2cursoudemyspringboot/Proyecto2CursoUdemySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto2cursoudemyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2CursoUdemySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto2CursoUdemySpringBootApplication.class, args);
+    }
+
+}
